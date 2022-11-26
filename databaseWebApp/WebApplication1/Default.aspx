9a832175e16f539cb02a594f24ff1519b29af13a
obliviamon <33405180+obliviamon@users.noreply.github.com>
2022-11-26 15:50:36 -0700

    Testing Connection

---
+++ databaseWebApp/WebApplication1/Default.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <asp:Button ID="Button1" runat="server" Text="Button" />
+        <asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+    </div>
+
+    </asp:Content>
---
+++ databaseWebApp/WebApplication1/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var cs = "Host=localhost;Username=postgres;Password=Darkstorm96;Database=postgres";
+            var con = new NpgsqlConnection(cs);
+            con.Open();
+
+            var sql = "SELECT version()";
+
+            var cmd = new NpgsqlCommand(sql, con);
+
+            var version = cmd.ExecuteScalar().ToString();
+            TextBox1.Text = ($"PostgreSQL version: {version}");
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
